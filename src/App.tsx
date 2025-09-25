22c3d1e931e0e04953335fb28a49f2104267f0e9
Sungho <hurgj123kr@gmail.com>
2025-09-25 23:32:35 +0900

    profolio setting

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Header } from "./components/Header";
+const App: React.FC = () => {
+  return (
+    <>
+      <div>
+        <Header />
+      </div>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+  return (
+    <div>
+      <h1>HSS'Portfolio</h1>
+    </div>
+  );
+};
