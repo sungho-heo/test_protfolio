115dd6e3249dd985f8bd0e59b61943e493d06b5c
Sungho <hurgj123kr@gmail.com>
2025-10-11 16:17:19 +0900

    footer css add and update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,8 @@
+import styles from "../scss/Footer.module.scss";
 export const Footer = () => {
   return (
-    <div>
-      <h1>footer</h1>
-    </div>
+    <footer className={styles.Footer_Details}>
+      &copy; {new Date().getFullYear()}. Heo Sung Ho All rights reserved
+    </footer>
   );
 };
