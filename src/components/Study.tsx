efc857629491e7ae440b1f5e753fdbdc1d2f91f8
Sungho <hurgj123kr@gmail.com>
2025-10-27 21:03:07 +0900

    projects2 detail add and study 리펙토링

--- src/components/Study.tsx
+++ src/components/Study.tsx
@@ -1,5 +1,39 @@
 import styles from "../scss/Study.module.scss";
 import globalStyles from "../scss/styles.module.scss";
+
+// type
+type StudyProps = {
+  id: number;
+  title: string;
+  url: string;
+  description: string;
+  tech: string[];
+};
+
+const studyList: StudyProps[] = [
+  {
+    id: 1,
+    title: "Movie web",
+    url: "https://github.com/sungho-heo/movieweb",
+    description: "aaaa",
+    tech: ["Javascript"],
+  },
+  {
+    id: 2,
+    title: "이것저것",
+    url: "aa",
+    description: "aaa",
+    tech: ["Javascript"],
+  },
+  {
+    id: 3,
+    title: "이것저것",
+    url: "aa",
+    description: "aaa2",
+    tech: ["Javascript"],
+  },
+];
+
 export const Study = () => {
   return (
     <>
@@ -8,57 +42,25 @@ export const Study = () => {
           <h2 className={globalStyles.SectionTitle_title}>STUDY</h2>
         </div>
         <div className={styles.Studies_lists}>
-          <div className={styles.StudyDetails_content}>
-            <div className={styles.StudyDetails_card}>
-              <h4 className={styles.StudyDetails_title}>
-                <a href="https://github.com/sungho-heo/movieweb">Movie web</a>
-              </h4>
-              <p className={styles.StudyDetails_description}>설명 부분</p>
-              <div>
-                <ul className={styles["StudyDetails_teach-stack"]}>
-                  <li>Javascript</li>
-                  <li>aaa</li>
-                  <li>a</li>
-                  <li>b</li>
-                  <li>c</li>
-                </ul>
+          {studyList.map((study) => (
+            <div className={styles.StudyDetails_content}>
+              <div className={styles.StudyDetails_card}>
+                <h4 className={styles.StudyDetails_title}>
+                  <a href={study.url}>{study.title}</a>
+                </h4>
+                <p className={styles.StudyDetails_description}>
+                  {study.description}
+                </p>
+                <div>
+                  <ul className={styles["StudyDetails_teach-stack"]}>
+                    {study.tech.map((text) => (
+                      <li>{text}</li>
+                    ))}
+                  </ul>
+                </div>
               </div>
             </div>
-          </div>
-          <div className={styles.StudyDetails_content}>
-            <div className={styles.StudyDetails_card}>
-              <h4 className={styles.StudyDetails_title}>
-                <a>이것저것</a>
-              </h4>
-              <p className={styles.StudyDetails_description}>설명 부분</p>
-              <div>
-                <ul className={styles["StudyDetails_teach-stack"]}>
-                  <li>Javascript</li>
-                  <li>aaa</li>
-                  <li>a</li>
-                  <li>b</li>
-                  <li>c</li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          <div className={styles.StudyDetails_content}>
-            <div className={styles.StudyDetails_card}>
-              <h4 className={styles.StudyDetails_title}>
-                <a>이것저것</a>
-              </h4>
-              <p className={styles.StudyDetails_description}>설명 부분</p>
-              <div>
-                <ul className={styles["StudyDetails_teach-stack"]}>
-                  <li>Javascript</li>
-                  <li>aaa</li>
-                  <li>a</li>
-                  <li>b</li>
-                  <li>c</li>
-                </ul>
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
       </section>
     </>
